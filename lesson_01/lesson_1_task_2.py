0f575005a072371ebc4a2708b55e99a7d8ce797f
Sveta Elshina <swetaels@rambler.ru>
2025-11-12 05:54:12 +0300

    Finish lesson1: basic Python tasks

---
+++ lesson_01/lesson_1_task_2.py
@@ -0,0 +1,3 @@
+my_age = 53
+my_age = 56
+print(my_age)
---
+++ lesson_01/training.py
@@ -0,0 +1,31 @@
+my_heigh = 167
+print(my_heigh)
+
+
+my_name = "Света"
+my_name = "Света Елшина"
+print(my_name)
+
+
+pet_name = input("Как зовут вашего питомца? ")
+print("Ваш любимчик - " + pet_name)
+
+
+def print_python():
+    print("Учу Python!")
+
+
+print_python()
+
+
+def print_letter(let):
+    print(let, end='')
+
+
+print_letter('C')
+print_letter('т')
+print_letter('у')
+print_letter('д')
+print_letter('е')
+print_letter('н')
+print_letter('т')
